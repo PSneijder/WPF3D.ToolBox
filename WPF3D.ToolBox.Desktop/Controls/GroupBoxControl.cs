78b2e9ef8b5c86cf6e84abf5bd77bada0b9961bc
Philip Schneider <philip.schneider@tdmsystems.com>
2017-01-15 23:25:30 +0100

    Renamed namespace.

--- WPF3D.ToolBox.Desktop/Controls/GroupBoxControl.cs
+++ WPF3D.ToolBox.Desktop/Controls/GroupBoxControl.cs
@@ -1,45 +1,41 @@
-﻿
+﻿using System.Windows;
+using System.Windows.Controls;
+
 namespace WPF3D.ToolBox.Controls
 {
-    using System.Windows;
-    using System.Windows.Controls;
-
-    namespace TDM.GL.Device.POC.Controls
-    {
-        public sealed class GroupBoxControl
+    public sealed class GroupBoxControl
             : ItemsControl
+    {
+        public static readonly DependencyProperty OrientationProperty = DependencyProperty.Register("Orientation", typeof(Orientation), typeof(GroupBoxControl), new PropertyMetadata(Orientation.Vertical));
+        public static readonly DependencyProperty ShowSeperatorProperty = DependencyProperty.Register("ShowSeperator", typeof(bool), typeof(GroupBoxControl), new PropertyMetadata(false));
+        public static readonly DependencyProperty HeaderProperty = DependencyProperty.Register("Header", typeof(string), typeof(GroupBoxControl), new PropertyMetadata(default(string)));
+
+        public Orientation Orientation
         {
-            public static readonly DependencyProperty OrientationProperty = DependencyProperty.Register("Orientation", typeof(Orientation), typeof(GroupBoxControl), new PropertyMetadata(Orientation.Vertical));
-            public static readonly DependencyProperty ShowSeperatorProperty = DependencyProperty.Register("ShowSeperator", typeof(bool), typeof(GroupBoxControl), new PropertyMetadata(false));
-            public static readonly DependencyProperty HeaderProperty = DependencyProperty.Register("Header", typeof(string), typeof(GroupBoxControl), new PropertyMetadata(default(string)));
+            get { return (Orientation)GetValue(OrientationProperty); }
+            set { SetValue(OrientationProperty, value); }
+        }
 
-            public Orientation Orientation
-            {
-                get { return (Orientation)GetValue(OrientationProperty); }
-                set { SetValue(OrientationProperty, value); }
-            }
+        public bool ShowSeperator
+        {
+            get { return (bool)GetValue(ShowSeperatorProperty); }
+            set { SetValue(ShowSeperatorProperty, value); }
+        }
 
-            public bool ShowSeperator
-            {
-                get { return (bool)GetValue(ShowSeperatorProperty); }
-                set { SetValue(ShowSeperatorProperty, value); }
-            }
+        public string Header
+        {
+            get { return (string)GetValue(HeaderProperty); }
+            set { SetValue(HeaderProperty, value); }
+        }
 
-            public string Header
-            {
-                get { return (string)GetValue(HeaderProperty); }
-                set { SetValue(HeaderProperty, value); }
-            }
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            return new ContentControl();
+        }
 
-            protected override DependencyObject GetContainerForItemOverride()
-            {
-                return new ContentControl();
-            }
-
-            protected override bool IsItemItsOwnContainerOverride(object item)
-            {
-                return false;
-            }
+        protected override bool IsItemItsOwnContainerOverride(object item)
+        {
+            return false;
         }
     }
 }
