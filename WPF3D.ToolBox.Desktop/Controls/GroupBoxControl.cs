4f4b84b1c90e17a4de12f19ad1b521f486dbb972
Philip Schneider <philip.schneider@tdmsystems.com>
2017-01-15 21:23:16 +0100

    Implemented GroupBoxControl.

---
+++ WPF3D.ToolBox.Desktop/Controls/GroupBoxControl.cs
@@ -0,0 +1,45 @@
+﻿
+namespace WPF3D.ToolBox.Controls
+{
+    using System.Windows;
+    using System.Windows.Controls;
+
+    namespace TDM.GL.Device.POC.Controls
+    {
+        public sealed class GroupBoxControl
+            : ItemsControl
+        {
+            public static readonly DependencyProperty OrientationProperty = DependencyProperty.Register("Orientation", typeof(Orientation), typeof(GroupBoxControl), new PropertyMetadata(Orientation.Vertical));
+            public static readonly DependencyProperty ShowSeperatorProperty = DependencyProperty.Register("ShowSeperator", typeof(bool), typeof(GroupBoxControl), new PropertyMetadata(false));
+            public static readonly DependencyProperty HeaderProperty = DependencyProperty.Register("Header", typeof(string), typeof(GroupBoxControl), new PropertyMetadata(default(string)));
+
+            public Orientation Orientation
+            {
+                get { return (Orientation)GetValue(OrientationProperty); }
+                set { SetValue(OrientationProperty, value); }
+            }
+
+            public bool ShowSeperator
+            {
+                get { return (bool)GetValue(ShowSeperatorProperty); }
+                set { SetValue(ShowSeperatorProperty, value); }
+            }
+
+            public string Header
+            {
+                get { return (string)GetValue(HeaderProperty); }
+                set { SetValue(HeaderProperty, value); }
+            }
+
+            protected override DependencyObject GetContainerForItemOverride()
+            {
+                return new ContentControl();
+            }
+
+            protected override bool IsItemItsOwnContainerOverride(object item)
+            {
+                return false;
+            }
+        }
+    }
+}
